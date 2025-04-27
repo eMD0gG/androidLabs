c04b05efb524779a930a4474b5db19c64da8a89c
eMD0gG <vaspyrkov2@gmail.com>
2025-04-28 01:31:41 +1000

    lab2 complete

--- app/src/main/java/com/example/androidlabs/MainActivity.kt
+++ app/src/main/java/com/example/androidlabs/MainActivity.kt
@@ -7,24 +7,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidlabs.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<Button>(R.id.registration_btn).setOnClickListener {
+        binding.registrationBtn.setOnClickListener {
             val intent: Intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
 
-        findViewById<Button>(R.id.login_btn).setOnClickListener {
+        binding.loginBtn.setOnClickListener {
             val intent: Intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/androidlabs/RegistrationActivity.kt
+++ app/src/main/java/com/example/androidlabs/RegistrationActivity.kt
@@ -1,5 +1,4 @@
 package com.example.androidlabs
-
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
@@ -9,37 +8,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidlabs.databinding.ActivityMainBinding
+import com.example.androidlabs.databinding.ActivityRegistrationBinding
 import com.google.android.material.appbar.MaterialToolbar
 
 class RegistrationActivity : AppCompatActivity() {
 
-    private lateinit var registrationInfo : TextView
+    private lateinit var binding: ActivityRegistrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registration)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val navBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val navBar = binding.topAppBar
 
         navBar.setNavigationOnClickListener {
             finish()
         }
 
-        registrationInfo = findViewById<TextView>(R.id.registration_info)
 
-        val text = registrationInfo.text.toString()
+        val text = binding.registrationInfo.text.toString()
         val spannable = SpannableString(text)
         val clickable = Clickable()
         spannable.setSpan(clickable, 47, 75, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         val clickable2 = Clickable()
         spannable.setSpan(clickable2, 128, 155, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        registrationInfo.text = spannable
+        binding.registrationInfo.text = spannable
     }
 }
